5d676153d283caf46cc861d64ba03c78f237047e
akshayraj08653-cell <akshayraj08653@gmail.com>
2026-07-16 13:38:04 +0530

    Fix Utils folder path case

--- routes/listing.js
+++ routes/listing.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const wrapAsync =require("../utils/wrapAsync.js");
+const wrapAsync =require("../Utils/wrapAsync.js");
 const Listing =require("../models/listing.js");
 const {isLoggedIn, isOwner,validateListing} = require("../middleware.js");
 const listingController = require("../controllers/listings");
