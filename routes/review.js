e82e521e253ad6a97714d83b2e9c6a7cb88cc58d
akshayraj08653-cell <akshayraj08653@gmail.com>
2026-07-16 13:47:36 +0530

    Fix Utils folder path case

--- routes/review.js
+++ routes/review.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router({mergeParams:true});
-const wrapAsync =require("../utils/wrapAsync.js");
-const ExpressError = require("../utils/ExpressError.js");
+const wrapAsync =require("../Utils/wrapAsync.js");
+const ExpressError = require("../Utils/ExpressError.js");
 const Review= require("../models/review.js");
 const Listing =require("../models/listing.js");
 const {validateReview, isLoggedIn, isReviewAuthor} = require("../middleware.js");
--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/user.js");
-const wrapAsync = require("../utils/wrapAsync");
+const wrapAsync = require("../Utils/wrapAsync");
 const passport = require("passport");
 const { required } = require("joi");
 const {saveRedirectUrl} = require ("../middleware");
